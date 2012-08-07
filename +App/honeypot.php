b02e6593d6eded68bb50766df9633ca8f5542346
srcspider <source.spider@gmail.com>
2012-08-07 20:59:15 +0300

    universal stash interface

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -10,7 +10,6 @@ class Stash_APC extends \ibidem\cache\Stash_APC { /** @return \ibidem\cache\Stas
 class Stash_Base extends \ibidem\cache\Stash_Base { /** @return \ibidem\cache\Stash_Base */ static function instance() { return parent::instance(); } }
 class Stash_File extends \ibidem\cache\Stash_File { /** @return \ibidem\cache\Stash_File */ static function instance() { return parent::instance(); } }
 class Stash_Memcached extends \ibidem\cache\Stash_Memcached { /** @return \ibidem\cache\Stash_Memcached */ static function instance() { return parent::instance(); } }
-class Stash extends \ibidem\cache\Stash {}
-class StashTags extends \ibidem\cache\StashTags { /** @return \ibidem\cache\StashTags */ static function instance() { return parent::instance(); } }
+class Stash extends \ibidem\cache\Stash { /** @return \ibidem\cache\Stash */ static function instance() { return parent::instance(); } }
 trait Trait_TaggedStash { use \ibidem\cache\Trait_TaggedStash; }
 class ViewStash extends \ibidem\cache\ViewStash {}
---
+++ Stash.php
@@ -0,0 +1,91 @@
+<?php namespace ibidem\cache;
+
+/**
+ * @package    ibidem
+ * @category   Cache
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class Stash extends \app\Instantiatable
+{
+	/**
+	 * Store a value under a key for a certain number of seconds.
+	 */
+	static function set($key, $data, $expires = null)
+	{
+		$class = '\app\Stash_'.\app\CFS::config('ibidem/cache')['default.cache'];
+		$class::set($key, $data, $expires);
+	}
+
+	/**
+	 * Retrieves data from $key
+	 * 
+	 * @return mixed data or default
+	 */
+	static function get($key, $default = null)
+	{
+		$class = '\app\Stash_'.\app\CFS::config('ibidem/cache')['default.cache'];
+		return $class::get($key, $default);
+	}
+
+	/**
+	 * Deletes $key
+	 */
+	static function delete($key)
+	{
+		$class = '\app\Stash_'.\app\CFS::config('ibidem/cache')['default.cache'];
+		$class::delete($key);
+	}
+	
+	/**
+	 * Stores data in $key, tagged with tags.
+	 */
+	static function store($key, $data, array $tags = [], $expires = null)
+	{
+		$class = '\app\Stash_'.\app\CFS::config('ibidem/cache')['default.cache'];
+		$class::store($key, $data, $tags, $expires);
+	}
+
+	/**
+	 * Deletes all caches tagged with $tags
+	 */
+	static function purge(array $tags)
+	{
+		$class = '\app\Stash_'.\app\CFS::config('ibidem/cache')['default.cache'];
+		$class::purge($tags);
+	}
+
+	/**
+	 * When timers is not provided the timers are retrieved automatically from 
+	 * the model. (timers act as invalidators for the stash)
+	 * 
+	 * Model must be provided in proper case. No prefix.
+	 * 
+	 * @return array tags for given parameters
+	 */
+	static function tags($model, array $timers = null)
+	{
+		// clean model; remove Model_ prefix is present
+		$model = \preg_replace
+			(
+				'#^Model_#', '', 
+				\join('', \array_slice(\explode('\\', $model), -1))
+			);
+		
+		if ($timers === null)
+		{
+			$class = '\app\Model_'.$model;
+			$timers = $class::timers();
+		}
+		
+		$tags = [];
+		foreach ($timers as $timer)
+		{
+			$tags[] = $model.'__'.$timer;
+		}
+		
+		return $tags;
+	}
+
+} # class
