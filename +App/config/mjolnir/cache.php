b01b6261da7ab245389d15a90bd730037b0be4de
srcspider <source.spider@gmail.com>
2012-09-10 03:58:28 +0300

    refactored names to mjolnir

---
+++ +App/config/mjolnir/cache.php
@@ -0,0 +1,50 @@
+<?php return array
+	(
+		'default.cache' => 'File',
+
+	## Cache settings
+	
+		'Memcached' => array
+			(
+				'lifetime.default' => 3600,
+				'compression' => true, // Use Zlib compression
+			
+				'timeout.recv' => 3000,
+				'timeout.send' => 1000,
+				'tcp.nodelay' => true,
+				'prefix' => 'memcached_',
+			
+				// giving instances a persistence id won't terminate
+				// the connection after use.
+				'persistent_id' => 'memcached_1',
+			
+				'servers' => array
+					(
+						array
+						(
+							'host' => 'localhost',
+							'port' => 11211,
+							'weight' => 1,
+							 
+						),
+					),
+			),
+	
+		'Memcache' => array
+			(
+				'lifetime.default' => 3600,				
+				'host' => 'localhost',
+				'port' => 11211,
+			),
+	
+		'APC' => array
+			(
+				'lifetime.default' => 3600,
+			),
+	
+		'File' => array
+			(
+				'cache.dir' => APPPATH.'cache'.DIRECTORY_SEPARATOR,
+				'lifetime.default' => 3600,
+			),
+	);
