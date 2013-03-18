82d95f3312cc0dab51d4871e26ae78df7796c61d
srcspider <source.spider@gmail.com>
2013-03-18 16:10:29 +0200

    disabled state, checks for Memcached and File

--- +App/config/mjolnir/require.php
+++ +App/config/mjolnir/require.php
@@ -1,11 +1,18 @@
 <?php namespace mjolnir\theme;
 
+$cache_config = \app\CFS::config('mjolnir/cache');
+
 return array
 	(
 		'mjolnir\cache' => array
 			(
-				 'extension=php_apc' => function ()
+				 'extension=php_apc' => function () use ($cache_config)
 					{
+						if ( ! $cache_config['APC']['enabled'])
+						{
+							return [ 'failed' => 'disabled' ];
+						}
+	
 						if (\extension_loaded('APC'))
 						{
 							return 'available';
@@ -14,14 +21,54 @@ return array
 						return 'failed';
 					},
 
-				'extension=php_memcache' => function ()
+				'extension=php_memcache' => function () use ($cache_config)
 					{
-						if(\class_exists('Memcache'))
+						if ( ! $cache_config['Memcache']['enabled'])
+						{
+							return [ 'failed' => 'disabled' ];
+						}
+						
+						if (\class_exists('Memcache'))
 						{
 							return 'available';
 						}
 
 						return 'failed';
-					}
+					},
+					
+				'extension=php_memcached' => function () use ($cache_config)
+					{
+						if ( ! $cache_config['Memcached']['enabled'])
+						{
+							return [ 'failed' => 'disabled' ];
+						}
+						
+						if (\class_exists('Memcached'))
+						{
+							return 'available';
+						}
+
+						return 'failed';
+					},
+							
+				'ETCPATH/cache' => function () use ($cache_config)
+					{
+						if ( ! $cache_config['File']['enabled'])
+						{
+							return [ 'failed' => 'disabled' ];
+						}
+						
+						if ( ! \file_exists(ETCPATH.'cache/'))
+						{
+							return 'error';
+						}
+						
+						if ( ! \is_writable(ETCPATH.'cache/'))
+						{
+							return 'error';
+						}
+
+						return 'available';
+					},
 			),
 	);
